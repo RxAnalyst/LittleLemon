0eaa2e25be692f185a91651697ffa93fbd5a1283
RxAnalyst <krishav.career@gmail.com>
2026-07-14 17:08:35 +0530

    created api for menu and booking

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -2,7 +2,39 @@
 
 # Create your views here.
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .serializers import UserSerializer, MenuSerializer
+from .models import Menu, Booking
+from .serializers import BookingSerializer
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+# Menu API - GET all menu items and POST new item
+class MenuView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuView(
+    generics.RetrieveUpdateAPIView,
+    generics.DestroyAPIView
+):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
